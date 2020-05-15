ffae55f56500a5ecdb05058eeee6a6dcf5eb1950
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-05-15 15:03:20 -0700

    it may have taken forever, but got a working sidebar

--- src/components/pages/dashboard.js
+++ src/components/pages/dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route, useRouteMatch, withRouter } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import clsx from 'clsx';
 import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -19,6 +20,7 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import { makeStyles } from '@material-ui/core/styles';
 import { withFirebase } from '../Firebase';
 import Sidebar from '../sidebar/index'
+import useStyles from '../../config/dashboard.config';
 // import Calendar from '../componenets/calendar';
 
 
@@ -36,99 +38,10 @@ function Copyright() {
   );
 }
 
-const drawerWidth = 240;
-
-const useStyles = makeStyles(theme => ({
-    root: {
-      display: 'flex',
-    },
-    toolbar: {
-      paddingRight: 24, 
-    },
-    toolbarIcon: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'flex-end',
-      padding: '0 8px',
-      ...theme.mixins.toolbar,
-    },
-    appBar: {
-      zIndex: theme.zIndex.drawer + 1,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    },
-    appBarShift: {
-      marginLeft: drawerWidth,
-      width: `calc(100% - ${drawerWidth}px)`,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    menuButton: {
-      marginRight: 36,
-    },
-    menuButtonHidden: {
-      display: 'none',
-    },
-    title: {
-      flexGrow: 1,
-    },
-    drawerPaper: {
-      position: 'relative',
-      whiteSpace: 'nowrap',
-      width: drawerWidth,
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    drawerPaperClose: {
-      overflowX: 'hidden',
-      transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      width: theme.spacing(7),
-      [theme.breakpoints.up('sm')]: {
-        width: theme.spacing(9),
-      },
-    },
-    appBarSpacer: theme.mixins.toolbar,
-    content: {
-      flexGrow: 1,
-      height: '100vh',
-      overflow: 'auto',
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    },
-    container: {
-      paddingTop: theme.spacing(4),
-      paddingBottom: theme.spacing(4),
-    },
-    contentClosed: {
-      marginLeft: 75
-    },
-    paper: {
-      padding: theme.spacing(2),
-      display: 'flex',
-      overflow: 'auto',
-      flexDirection: 'column',
-    },
-    fixedHeight: {
-      height: 240,
-    },
-  }));
   
 
-
-
 function Dashboard(props) {
-    let match = useRouteMatch();
+let match = useRouteMatch();
 
 
   const classes = useStyles();
@@ -140,10 +53,10 @@ function Dashboard(props) {
   const handleDrawerClose = () => setOpen(false);
 
 
-//   const signOut = () => {
-//       props.firebase.auth.signOut()
-//       props.history.push("/");
-//   }
+  const signOut = () => {
+      props.firebase.auth.signOut()
+      props.history.push("/");
+  }
 
   return (
     //   <AuthUserContext.Consumer>
@@ -172,10 +85,11 @@ function Dashboard(props) {
           </IconButton>
         </Toolbar>
       </AppBar>
-{/* 
-    <Sidebar 
 
-    /> */}
+    <Sidebar 
+    signOut={signOut} open={open} handleDrawerClose={handleDrawerClose}
+
+    />
 
      
       <main className={classes.content}>
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,74 +1,78 @@
-// import React from 'react';
-// import { Link, useRouteMatch } from 'react-router-dom';
-// import ListItem from '@material-ui/core/ListItem';
-// import ListItemIcon from '@material-ui/core/ListItemIcon';
-// import ListItemText from '@material-ui/core/ListItemText';
-// import ListSubheader from '@material-ui/core/ListSubheader';
-// import EventNoteIcon from '@material-ui/icons/EventNote';
-// import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-// import SettingsIcon from '@material-ui/icons/Settings';
+import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import SettingsIcon from '@material-ui/icons/Settings';
 
-// import clsx from 'clsx';
-// import Drawer from '@material-ui/core/Drawer';
-// import IconButton from '@material-ui/core/IconButton';
-// import List from '@material-ui/core/List';
-// import Divider from '@material-ui/core/Divider';
-// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-// import useStyles from '../pages/dashboard';
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import  useStyles from './../../config/dashboard.config';
 
 
-// function Sidebar(props) {
-//     let match = useRouteMatch();
+function Sidebar(props) {
+    let match = useRouteMatch();
 
-//     const classes = useStyles();
+    const classes = useStyles();
 
-//     return (
-//         <Drawer
-//             variant="permanent"
-//             classes={{
-//                 paper: clsx(classes.drawerPaper, !props.open && classes.drawerPaperClose),
-//             }}
-//             open={props.open}
-//             >
-//             <div className={classes.toolbarIcon}>
-//                 <IconButton onClick={props.handleDrawerClose}>
-//                 <ChevronLeftIcon />
-//                 </IconButton>
-//             </div>
-//             <Divider />
-//             <List>
-//                 <ListSubheader inset>Menu</ListSubheader>
-//                 <Link to={`${match.url}`}>
-//                     <ListItem button>
-//                         <ListItemIcon>
-//                             <EventNoteIcon />
-//                         </ListItemIcon>
-//                         <ListItemText primary="Workouts" />
-//                     </ListItem>
-//                 </Link>
-//             </List>
-//             <Divider />
-//             <List>
-//                 <div>
-//                     <ListSubheader inset>Account</ListSubheader>
-//                     <Link to={`${match.url}/settings`}>
-//                         <ListItem button>
-//                             <ListItemIcon>
-//                                 <SettingsIcon />
-//                             </ListItemIcon>
-//                             <ListItemText primary="Settings" />
-//                         </ListItem>
-//                     </Link>
-//                     <ListItem button onClick={() => props.signOut()}>
-//                         <ListItemIcon>
-//                             <ExitToAppIcon />
-//                         </ListItemIcon>
-//                         <ListItemText primary="Log out" />
-//                     </ListItem>
-//                 </div>
-//             </List>
-//         </Drawer>
-//     );
-// }
+    
 
-// export default Sidebar;
+
+
+    return (
+        <Drawer
+            variant="permanent"
+            classes={{
+                paper: clsx(classes.drawerPaper, !props.open && classes.drawerPaperClose),
+            }}
+            open={props.open}
+            >
+            <div className={classes.toolbarIcon}>
+                <IconButton onClick={props.handleDrawerClose}>
+                <ChevronLeftIcon />
+                </IconButton>
+            </div>
+            <Divider />
+            <List>
+                <ListSubheader inset>Menu</ListSubheader>
+                <Link to={`${match.url}`}>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <EventNoteIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Workouts" />
+                    </ListItem>
+                </Link>
+            </List>
+            <Divider />
+            <List>
+                <div>
+                    <ListSubheader inset>Account</ListSubheader>
+                    <Link to={`${match.url}/settings`}>
+                        <ListItem button>
+                            <ListItemIcon>
+                                <SettingsIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Settings" />
+                        </ListItem>
+                    </Link>
+                    <ListItem button onClick={() => props.signOut()}>
+                        <ListItemIcon>
+                            <ExitToAppIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Log out" />
+                    </ListItem>
+                </div>
+            </List>
+        </Drawer>
+    );
+}
+
+export default Sidebar;
