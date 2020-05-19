66221bc464de33525e7673cc4fbe02fa5bd3e91b
Zakkrey Short <zakkreyjshort@gmail.com>
2020-05-19 13:49:14 -0700

    creating form for edit workout

--- src/components/EditWorkout/EditWorkout.js
+++ src/components/EditWorkout/EditWorkout.js
@@ -9,3 +9,60 @@ import FormControl from '@material-ui/core/FormControl';
 import Slider from '@material-ui/core/Slider';
 import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles(theme => ({
+    formControl: {
+        minWidth: '100%',
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
+
+function EditWorkout(props) {
+    const classes = useStyles();
+
+    const {authUser, firebase, workout, workoutKey, setEditing, setOpenSnackbar, setSnackbarMsg} = props;
+    const uid = authUser.uid;
+
+    const defaultWorkout = {
+        name: workout.name,
+        type: workout.type,
+        duration: workout.duration,
+        date: workout.date
+    }
+
+    const [newWorkout, setNewWorkout] = useState(defaultWorkout);
+
+    const handleChange = e => {
+        const { name, value } = e.target
+        setNewWorkout({
+            ...newWorkout,
+            [name]: value
+        });
+    }
+
+    const handleSlider = e => {
+        const duration = e.target.getAttribute('aria-valuenow');
+        setNewWorkout({...newWorkout, duration: duration});
+    }
+
+    const isValidWorkout = newWorkout.name === '';
+
+    const handleSubmit = action => {
+        if (authUser){
+            firebase.updateWorkout(uid, newWorkout, workoutKey);
+            setEditing(false);
+            setOpenSnackbar(true);
+            setSnackbarMsg('Updated workout');
+            setTimeout(() => {
+                setOpenSnackbar(false)
+            }, 4000)
+        };
+    }
+
+    return (
+        <form>
+            
+        </form>
+    )
+}
