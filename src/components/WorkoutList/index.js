ea39ccc1482e8c13f878efb728a58eca5fd4eb98
Zakkrey Short <zakkreyjshort@gmail.com>
2020-05-19 11:25:38 -0700

    bring in imports for workout list

---
+++ src/components/WorkoutList/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withFirebase } from '../Firebase';
+import loader from './loader.gif'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import DeleteIcon from '@material-ui/core/Delete';
+import EditIcon from '@material-ui/core/Edit';
