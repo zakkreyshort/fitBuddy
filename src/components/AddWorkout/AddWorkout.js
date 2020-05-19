e71f6db016533fe6572ac67971e133fd6d8e74eb
Zakkrey Short <zakkreyjshort@gmail.com>
2020-05-19 09:05:02 -0700

    change calendar color, update add workout component with imports

--- src/components/AddWorkout/AddWorkout.js
+++ src/components/AddWorkout/AddWorkout.js
@@ -1,3 +1,14 @@
+import React from 'react';
+import { withFirebase } from '../Firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Slider from '@material-ui/core/Slider';
+import Typography from '@material-ui/core/Typography';
+
 
 
 const AddWorkout = () => {
