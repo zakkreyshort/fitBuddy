19efa723209a0dc5d9a6244160badf3b48d891a9
Zakkrey Short <zakkreyjshort@gmail.com>
2020-05-20 10:42:40 -0700

    create componenets for dark mode handling

--- src/components/darkmode/darkmode.js
+++ src/components/darkmode/darkmode.js
@@ -1,13 +1,12 @@
 export const lightTheme = {
-  body: '#E2E2E2',
+  body: '#FFF',
   text: '#363537',
   toggleBorder: '#FFF',
-  gradient: 'linear-gradient(#39598A, #79D7ED)',
+  background: '#363537',
 }
-
 export const darkTheme = {
   body: '#363537',
   text: '#FAFAFA',
   toggleBorder: '#6B8096',
-  gradient: 'linear-gradient(#091236, #1E215D)',
+  background: '#999',
 }
