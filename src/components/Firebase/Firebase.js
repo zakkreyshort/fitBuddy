5e820ffe8d5ec87a5bb3bcb5c494b1bda88fd7df
Zakkrey Short <zakkreyjshort@gmail.com>
2020-05-20 11:56:04 -0700

    remove initial db

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -2,7 +2,6 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
-// below is code for secondary firebase
 var firebaseConfig = {
   apiKey: "AIzaSyB-3uE79aaDOj3pQnkibRsfJUXtZhXWEWc",
   authDomain: "fit-buddy-5e92b.firebaseapp.com",
@@ -14,16 +13,6 @@ var firebaseConfig = {
   measurementId: "G-VFWZXFQ6VM"
 };
 
-// var firebaseConfig = {
-//     apiKey: "AIzaSyCF80aVPSwldshmZhuATsa5t2hKk5YDeSU",
-//     authDomain: "fitbuddy-77dfc.firebaseapp.com",
-//     databaseURL: "https://fitbuddy-77dfc.firebaseio.com",
-//     projectId: "fitbuddy-77dfc",
-//     storageBucket: "fitbuddy-77dfc.appspot.com",
-//     messagingSenderId: "613913011935",
-//     appId: "1:613913011935:web:e3e0398ac5e94556f1eb6a",
-//     measurementId: "G-148J9C82XH"
-//   };
 
   class Firebase {
     constructor() {
