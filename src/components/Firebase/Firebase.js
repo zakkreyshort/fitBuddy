d1ac9a99900dc0e8d9463d1ee197c7cfb986491b
Zakkrey Short <zakkreyjshort@gmail.com>
2020-05-20 09:26:41 -0700

    make new firebase db, connect, searching for error index 84

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -2,16 +2,28 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
+// below is code for secondary firebase
 var firebaseConfig = {
-    apiKey: "AIzaSyCF80aVPSwldshmZhuATsa5t2hKk5YDeSU",
-    authDomain: "fitbuddy-77dfc.firebaseapp.com",
-    databaseURL: "https://fitbuddy-77dfc.firebaseio.com",
-    projectId: "fitbuddy-77dfc",
-    storageBucket: "fitbuddy-77dfc.appspot.com",
-    messagingSenderId: "613913011935",
-    appId: "1:613913011935:web:e3e0398ac5e94556f1eb6a",
-    measurementId: "G-148J9C82XH"
-  };
+  apiKey: "AIzaSyB-3uE79aaDOj3pQnkibRsfJUXtZhXWEWc",
+  authDomain: "fit-buddy-5e92b.firebaseapp.com",
+  databaseURL: "https://fit-buddy-5e92b.firebaseio.com",
+  projectId: "fit-buddy-5e92b",
+  storageBucket: "fit-buddy-5e92b.appspot.com",
+  messagingSenderId: "630651897979",
+  appId: "1:630651897979:web:3d970dd60cfe2536bf3ade",
+  measurementId: "G-VFWZXFQ6VM"
+};
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCF80aVPSwldshmZhuATsa5t2hKk5YDeSU",
+//     authDomain: "fitbuddy-77dfc.firebaseapp.com",
+//     databaseURL: "https://fitbuddy-77dfc.firebaseio.com",
+//     projectId: "fitbuddy-77dfc",
+//     storageBucket: "fitbuddy-77dfc.appspot.com",
+//     messagingSenderId: "613913011935",
+//     appId: "1:613913011935:web:e3e0398ac5e94556f1eb6a",
+//     measurementId: "G-148J9C82XH"
+//   };
 
   class Firebase {
     constructor() {
